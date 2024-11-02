c6ec35476eedb234fbc619595b5c7110fc089f68
Anish V Mathew <anishvmathew02@gmail.com>
2024-11-02 19:03:44 +0530

    gym owner pages changed

--- views/gym-owner/gymimage.php
+++ views/gym-owner/gymimage.php
@@ -7,7 +7,7 @@
   <title>Image Upload Form</title>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/stylesheets/gymdashboard.css">
+  <link rel="stylesheet" href="/public/stylesheets/gymdashboard.css">
 </head>
 
 <body>
@@ -76,7 +76,7 @@
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="/javascript/gymowner.js"></script>
+  <script src="/public/javascript/gymowner.js"></script>
 </body>
 
 </html>
